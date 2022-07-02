036965ccf8e75cbdd01c6acc21c4967e8a2d4258
Evgeni <eugenluzin@gmail.com>
2022-07-02 16:38:32 +0500

    Добавил движение кубика

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -20,10 +20,50 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static Vector _moveVector = new (0,0);
+        private const int _moveSpeed = 5;
         public MainWindow()
         {
             InitializeComponent();
-            Box.Background = Brushes.Black;
+            MoveSnake();
+        }
+
+        async void MoveSnake()
+        {
+            while (true)
+            {
+                int height = (int)Canvas.GetTop(Rect);
+                Canvas.SetTop(Rect, height + _moveVector.Y);
+                int width = (int)Canvas.GetLeft(Rect);
+                Canvas.SetLeft(Rect, width + _moveVector.X);
+                await Task.Delay(10);
+            }
+        }
+
+        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    _moveVector.X = 0;
+                    _moveVector.Y = -_moveSpeed;
+                    break;
+                case Key.Down:
+                    _moveVector.X = 0;
+                    _moveVector.Y = _moveSpeed;
+                    break;
+                case Key.Right:
+                    _moveVector.X = _moveSpeed;
+                    _moveVector.Y = 0;
+                    break;
+                case Key.Left:
+                    _moveVector.X = -_moveSpeed;
+                    _moveVector.Y = 0;
+                    break;
+                default:
+                    break;
+            }
+            
         }
     }
 }
--- Snake/SnakeElement.cs
+++ Snake/SnakeElement.cs
@@ -1,5 +1,13 @@
-﻿namespace Snake;
+﻿
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace Snake;
 
 public class SnakeElement
 {
+    private Rectangle _snakeRectangle;
+    private Point _snakePosition;
+    private SnakeElement _childSnakeElement;
+    private Vector _snakeMotionVector;
 }
